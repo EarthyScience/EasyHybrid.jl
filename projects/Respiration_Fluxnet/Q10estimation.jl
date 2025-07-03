9984e21952665072ea0456c9fa635f65d83feef8
BernhardAhrens <bahrens@bgc-jena.mpg.de>
2025-07-03 21:14:54 +0200

    nicer legend

--- projects/Respiration_Fluxnet/Q10estimation.jl
+++ projects/Respiration_Fluxnet/Q10estimation.jl
@@ -18,22 +18,25 @@ GLMakie.activate!(inline=false)  # use non-inline (external) window for plots
 # Create a figure and plot RECO_NT and RECO_DT time series
 fig1 = Figure()
 
-fig1[1, 1] = Makie.Axis(fig1; xlabel = "Time", title = "RECO")
+ax1 = fig1[1, 1] = Makie.Axis(fig1; ylabel = "RECO")
 lines!(site.timeseries.time, site.timeseries.RECO_NT, label = "RECO_NT")
 lines!(site.timeseries.time, site.timeseries.RECO_DT, label = "RECO_DT")
-axislegend(position = :rt)
+fig1[1, 2] = Legend(fig1, ax, framevisible = false)
+hidexdecorations!()
 
-fig1[2,1] = Makie.Axis(fig1; xlabel = "Time", title = "SWC")
+fig1[2,1] = Makie.Axis(fig1; ylabel = "SWC")
 lines!(site.timeseries.time, site.timeseries.SWC_shallow, label = "SWC_shallow")
+hidexdecorations!()
 
-fig1[3,1] = Makie.Axis(fig1; xlabel = "Time", title = "Precipitation")
+fig1[3,1] = Makie.Axis(fig1; ylabel = "Precipitation")
 lines!(site.timeseries.time, site.timeseries.P, label = "P")
+hidexdecorations!()
 
-fig1[4,1] = Makie.Axis(fig1; xlabel = "Time", title = "Temperature")
+ax4 = fig1[4,1] = Makie.Axis(fig1; xlabel = "Time", ylabel = "Temperature")
 lines!(site.timeseries.time, site.timeseries.TA, label = "air")
 lines!(site.timeseries.time, site.timeseries.TS_shallow, label = "soil")
+fig1[4, 2] = Legend(fig1, ax4, framevisible = false)
 
-axislegend(position = :rt)
 linkxaxes!(filter(x -> x isa Axis, fig1.content)...)
 
 fig1
