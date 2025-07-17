3257c1850785aff932932e098825ed1930adef9a
Meng2077 <tianxuemeng1211@hotmail.com>
2025-07-17 15:34:41 +0200

    try parameter search

--- projects/BulkDSOC/parameter_search.jl
+++ projects/BulkDSOC/parameter_search.jl
@@ -3,6 +3,7 @@ Pkg.activate("projects/BulkDSOC")
 Pkg.develop(path=pwd())
 Pkg.instantiate()
 
+using AxisKeys
 using Revise
 using EasyHybrid
 using Lux
@@ -15,59 +16,79 @@ using EasyHybrid.MLUtils
 using Statistics
 using Plots
 using Flux
+using NNlib
 
-# 01 - univariate naive NN
+# 00 - parameter space search 
 testid = "00_search"
 
-# input
 df = CSV.read(joinpath(@__DIR__, "./data/lucas_preprocessed.csv"), DataFrame, normalizenames=true);
-df_d = dropmissing(df); # complete SOCD
-# BD g/cm3 [0,2.2], SOCconc [0,1], CF [0,1], SOCdensity ton/m3
-tgt = :SOCconc; # , :CF, :SOCdensity
-df_d.SOCconc .= log.(df_d.SOCconc .* 1000 .+ 1); # to log1p(permille)
-min = minimum(df_d.SOCconc)
-max = maximum(df_d.SOCconc)
-df_d.SOCconc .= (df_d.SOCconc .- min) ./ (max - min); # normalize to [0,1]
+df_d = dropmissing(df);
+df_d .= Float32.(df_d);
+tgt = :SOCconc;
+df_d.SOCconc .= log.(df_d.SOCconc .* 1000 .+ 1);
+minv = minimum(df_d.SOCconc)
+maxv = maximum(df_d.SOCconc)
+df_d.SOCconc .= (df_d.SOCconc .- minv) ./ (maxv - minv);
 
-hidden_configs = [
-    (128, 64, 16),
+# predictors/targets
+predictors = Symbol.(names(df_d))[4:end-1];
+target = [tgt];
+ka = to_keyedArray(df_d);
+
+# parameter space
+nf = length(predictors)
+hidden_configs = [  
+    (32, 16),
+    # (256, 128, 64, 32, 16),
+    (256, 128, 64, 32),
     (256, 128, 64),
+    (256, 128),
+    (128, 64, 32, 16),
+    (128, 64, 32),
+    (128, 64),
+    (64, 32, 16),
     (64, 32)
 ]
-batch_sizes = [64, 128, 256]
-epochs_list = [100, 200]
-lrs = [1e-3, 5e-4, 1e-4]
+batch_sizes = [32, 64, 128, 256, 512];
+lrs = [1e-2, 1e-3, 1e-4];
+activations = [relu, tanh, swish, gelu];
 
 results = []
 
-for h in hidden_configs, bs in batch_sizes, nepochs in epochs_list, lr in lrs
-    println("Testing h=$h, bs=$bs, epochs=$nepochs, lr=$lr")
-    
-    # build NN dynamically
-    NN = EasyHybrid.constructNNModel(predictors, targets; hidden_layers=Chain(Dense(32, 32, tanh), Dense(32, 16, tanh)), scale_nn_outputs=false)
-    ps, st = LuxCore.setup(Random.default_rng(), model)
+for h in hidden_configs, bs in batch_sizes, lr in lrs, act in activations
+    println("Testing h=$h, bs=$bs, lr=$lr, activation=$act")
 
-    layers = []
-    push!(layers, Dense(nfeatures, h[1], relu))
-    for i in 2:length(h)
-        push!(layers, Dense(h[i-1], h[i], relu))
-    end
-    push!(layers, Dense(h[end], 1))
-    NN = Chain(layers...)
+    nn = EasyHybrid.constructNNModel(
+        predictors, target;
+        hidden_layers = collect(h),     
+        activation = act,
+        scale_nn_outputs = false
+    )
 
-    # train
-    result = train(NN, (ds_p, y); nepochs=nepochs, batchsize=bs, opt=AdamW(lr))
-    
-    # evaluate (reuse your code)
-    y_val_true = vec(result[:y_val])
-    y_val_pred = vec(result[:ŷ_val])
-    ss_res = sum((y_val_true .- y_val_pred).^2)
-    ss_tot = sum((y_val_true .- mean(y_val_true)).^2)
-    r2 = 1 - ss_res / ss_tot
+    result = train(
+        nn,
+        ka,                 
+        ();
+        nepochs = 200,
+        batchsize = bs,
+        opt = AdamW(lr),
+        training_loss = :mse,
+        loss_types = [:mse, :r2],
+        shuffleobs = true,
+        file_name = nothing # skip for now
+    )
 
-    push!(results, (h, bs, nepochs, lr, r2))
+    # metrics
+    best_epoch = argmax([vh.r2.sum for vh in result.val_history])
+    best_val = result.val_history[best_epoch]
+
+    vh = result.val_history[1]
+    @show vh
+    @show vh.r2
+
+    push!(results, (h, bs, lr, act, best_val.r2, best_val.mse, best_epoch))
 end
 
-# Sort and show best
-best = sort(results, by=x->x[end], rev=true)[1]
+
+best = sort(results, by = x -> x[end], rev = true)[1]
 println("Best: ", best)
