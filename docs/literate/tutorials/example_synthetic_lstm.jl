baa71703224ff7d80ec7da3614e567e7888a953f
BernhardAhrens <32769547+BernhardAhrens@users.noreply.github.com>
2025-12-27 21:57:58 +0100

    Update docs/literate/tutorials/example_synthetic_lstm.jl
    Co-authored-by: Lazaro Alonso <lazarus.alon@gmail.com>

--- docs/literate/tutorials/example_synthetic_lstm.jl
+++ docs/literate/tutorials/example_synthetic_lstm.jl
@@ -23,7 +23,7 @@ using Lux
 # ## 2. Data Loading and Preprocessing
 
 # Load synthetic dataset from GitHub
-df = load_timeseries_netcdf("https://github.com/bask0/q10hybrid/raw/master/data/Synthetic4BookChap.nc")
+df = load_timeseries_netcdf("https://github.com/bask0/q10hybrid/raw/master/data/Synthetic4BookChap.nc");
 
 # Select a subset of data for faster execution
 df = df[1:20000, :]
