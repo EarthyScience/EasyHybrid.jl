5fd44747db9dd3dba91cd2ca0d55f7ca0fa69175
Lazaro Alonso <lazarus.alon@gmail.com>
2026-04-22 09:33:05 +0200

    don't mention gpu for now

--- docs/make.jl
+++ docs/make.jl
@@ -63,9 +63,9 @@ makedocs(;
             "Hyperparameter Tuning" => "tutorials/hyperparameter_tuning.md",
             "GPU Acceleration" => "tutorials/gpu.md",
             "Synthetic Respiration on GPU" => "tutorials/synthetic_respiration_gpu.md",
-            "Slurm" => "tutorials/slurm.md",
+            # "Slurm" => "tutorials/slurm.md",
             "Cross-validation" => "tutorials/folds.md",
-            "LSTM Hybrid Model" => "tutorials/example_synthetic_lstm.md",
+            # "LSTM Hybrid Model" => "tutorials/example_synthetic_lstm.md",
             "Loss Functions" => "tutorials/losses.md",
         ],
         "Research" => [
