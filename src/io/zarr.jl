6608fa32bf138c0f4262b4d0c3f85c5ea0aac738
Lazaro Alonso <lazarus.alon@gmail.com>
2026-09-23 09:16:19 +0200

    call reconstruct_layer instead

--- src/io/zarr.jl
+++ src/io/zarr.jl
@@ -148,7 +148,7 @@ function _reconstruct_hybrid_model(info::AbstractDict; mechanistic_model = nothi
     )
 end
 
-function LuxZarr._reconstruct_layer(::Val{:HybridModel}, info::AbstractDict; mechanistic_model = nothing, kwargs...)
+function LuxZarr.reconstruct_layer(::Val{:HybridModel}, info::AbstractDict; mechanistic_model = nothing, kwargs...)
     return _reconstruct_hybrid_model(info; mechanistic_model = mechanistic_model, kwargs...)
 end
 
@@ -160,7 +160,7 @@ function LuxZarr.extract_model_info(m::InputBatchNorm)
     )
 end
 
-function LuxZarr._reconstruct_layer(::Val{:InputBatchNorm}, info::AbstractDict; kwargs...)
+function LuxZarr.reconstruct_layer(::Val{:InputBatchNorm}, info::AbstractDict; kwargs...)
     layer_info = get(info, "layer", nothing)
     layer = reconstruct_model_from_info(layer_info; kwargs...)
     layer === nothing && return nothing
@@ -175,7 +175,7 @@ function LuxZarr.extract_model_info(m::RecurrenceOutputDense)
     )
 end
 
-function LuxZarr._reconstruct_layer(::Val{:RecurrenceOutputDense}, info::AbstractDict; kwargs...)
+function LuxZarr.reconstruct_layer(::Val{:RecurrenceOutputDense}, info::AbstractDict; kwargs...)
     layer_info = get(info, "layer", nothing)
     layer = reconstruct_model_from_info(layer_info; kwargs...)
     layer === nothing && return nothing
